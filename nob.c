acbc63936826f3bb1411f21724db25302a8f7f90
celyk <humanimations10@gmail.com>
2026-07-26 19:42:08 +1000

    Update

--- nob.c
+++ nob.c
@@ -39,18 +39,12 @@ void configure() {
 
 #ifdef PLATFORM_IOS
 
-    nob_cmd_append(&cmd, "sudo", "apt", "install", 
-        "libx11-dev", 
-        "libxi-dev", 
-        "libxcursor-dev", 
-        "mesa-common-dev", 
-        "libgl1-mesa-dri", 
-        "libgbm1"
-    );
+    nob_cmd_append(&cmd, "sudo", "xcode-select", "--install");
 
     if (!nob_cmd_run(&cmd)) {
         exit(1);
     }
+
 #endif /* PLATFORM_LINUX */
 
     if (cmd.count) {
