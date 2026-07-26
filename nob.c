e4131fe0588d65823dad804018a801ff723c17f3
celyk <humanimations10@gmail.com>
2026-07-26 18:40:23 +1000

    Update

--- nob.c
+++ nob.c
@@ -23,16 +23,17 @@ void configure() {
 
 #ifdef PLATFORM_LINUX
     nob_cmd_append(&cmd, "sudo", "apt", "install", "mesa-common-dev");
-    nob_cmd_append(&cmd, "&&", "sudo", "apt", "install", "libxi-dev");
+    //nob_cmd_append(&cmd, "&&", "sudo", "apt", "install", "libxi-dev");
 
     if (!nob_cmd_run(&cmd)) {
         exit(1);
     }
 
+    nob_cmd_append(&cmd, "sudo", "apt", "install", "libxi-dev");
     // nob_cmd_append(&cmd, "sudo", "apt", "install", "libx11-dev");
-    // if (!nob_cmd_run(&cmd)) {
-    //     exit(1);
-    // }
+    if (!nob_cmd_run(&cmd)) {
+        exit(1);
+    }
 #endif
 
     if (cmd.count) {
