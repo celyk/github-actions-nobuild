0815a0f9b37c79d50d41f9b36f30c55db77a0a8f
celyk <humanimations10@gmail.com>
2026-07-26 16:50:30 +1000

    Update

--- nob.c
+++ nob.c
@@ -17,24 +17,7 @@
 #define BUILD_SUBFOLDER "build/web/"
 #endif
 
-int main(int argc, char **argv)
-{
-    // This line enables the self-rebuilding. It detects when nob.c is updated and auto rebuilds it then
-    // runs it again.
-    NOB_GO_REBUILD_URSELF(argc, argv);
-
-    // It's better to keep all the building artifacts in a separate build folder. Let's create it if it
-    // does not exist yet.
-    //
-    // Majority of the nob command return bool which indicates whether operation has failed or not (true -
-    // success, false - failure). If the operation returned false you don't need to log anything, the
-    // convention is usually that the function logs what happened to itself. Just do
-    // `if (!nob_function()) return;`
-    if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
-    if (!nob_mkdir_if_not_exists(BUILD_SUBFOLDER)) return 1;
-
-    // The working horse of nob is the Nob_Cmd structure. It's a Dynamic Array of strings which represent
-    // command line that you want to execute.
+int buildExe() {
     Nob_Cmd cmd = {0};
 
     nob_cc(&cmd);
@@ -47,6 +30,35 @@ int main(int argc, char **argv)
 #endif
 
     if (!nob_cmd_run(&cmd)) return 1;
+
+    return 0;
+}
+
+int runExe() {
+    Nob_Cmd cmd = {0};
+
+#if !defined(PLATFORM_WINDOWS)
+    nob_cmd_append(&cmd, "cd", BUILD_SUBFOLDER);
+    nob_cmd_append(&cmd, "&&", "./main");
+
+#else
+    nob_cmd_append(&cmd, "cd", BUILD_SUBFOLDER);
+    nob_cmd_append(&cmd, "&&", "main.exe");
+#endif
+
+    if (!nob_cmd_run(&cmd)) return 1;
+
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    // This line enables the self-rebuilding. It detects when nob.c is updated and auto rebuilds it then
+    // runs it again.
+    NOB_GO_REBUILD_URSELF(argc, argv);
+
+    if(buildExe() != 0) { return 1; };
+    if(runExe() != 0) { return 1; };
 
     return 0;
 }
