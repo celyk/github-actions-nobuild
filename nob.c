864a812f680a803c17d489a864d1321f65f149a9
celyk <humanimations10@gmail.com>
2026-07-26 19:59:00 +1000

    Update

--- nob.c
+++ nob.c
@@ -40,8 +40,9 @@ void configure() {
 #ifdef TARGET_PLATFORM_IOS
 
     //nob_cmd_append(&cmd, "sudo", "xcode-select", "--install");
-    nob_cmd_append(&cmd, "xcodebuild", "-showsdks");
-
+    //nob_cmd_append(&cmd, "xcodebuild", "-showsdks");
+    nob_cmd_append(&cmd, "ls", "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/‌​SDKs/");
+    
     if (!nob_cmd_run(&cmd)) {
         exit(1);
     }
