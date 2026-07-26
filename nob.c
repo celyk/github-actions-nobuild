0e4c5e298f7a6d9db2e4f1546df9878fde7adefd
celyk <humanimations10@gmail.com>
2026-07-26 18:31:03 +1000

    Update

--- nob.c
+++ nob.c
@@ -42,9 +42,7 @@ int buildExe() {
     nob_cc_flags(&cmd);
     nob_cc_inputs(&cmd, "-I", INCLUDES_FOLDER);
 
-
     nob_cc_output(&cmd, BUILD_SUBFOLDER "main");
-    nob_cc_inputs(&cmd, SRC_FOLDER "main.c");
 
 #ifdef PLATFORM_DARWIN
     nob_cc_inputs(&cmd, "-x", "objective-c");
@@ -67,6 +65,7 @@ int buildExe() {
     // nob_cmd_append(&cmd, "-lopengl32");
 #endif /* PLATFORM_DARWIN */
 
+    nob_cc_inputs(&cmd, SRC_FOLDER "main.c");
 
 #if defined(PLATFORM_GITHUB_WORKFLOW)
     nob_cmd_append(&cmd, "-DPLATFORM_GITHUB_WORKFLOW");
