94af713fbb6ab4b005677ab7c9c3317428038d20
celyk <humanimations10@gmail.com>
2026-07-26 16:35:44 +1000

    Update

---
+++ includes/platform_defs.h
@@ -0,0 +1,39 @@
+#if defined(__APPLE__)
+    #if !defined(__cplusplus)
+    #endif
+
+    #include <TargetConditionals.h>
+    #if defined(TARGET_OS_IPHONE) && !TARGET_OS_IPHONE
+        // MacOS
+        #define PLATFORM_MACOS
+    #else
+        // iOS or iOS Simulator
+        #define PLATFORM_IOS
+    #endif
+#elif defined(__EMSCRIPTEN__)
+    #define PLATFORM_WEB
+#elif defined(_WIN32)
+    #define PLATFORM_WINDOWS
+#elif defined(__ANDROID__)
+    #define PLATFORM_ANDROID
+#elif defined(__linux__) || defined(__unix__)
+    #define PLATFORM_LINUX
+#else
+#error "platform_defs.h: Unknown platform"
+#endif
+
+#if defined(PLATFORM_MACOS) || defined(PLATFORM_LINUX) || defined(PLATFORM_WINDOWS)
+    #define PLATFORM_DESKTOP
+#endif
+
+#if defined(PLATFORM_IOS) || defined(PLATFORM_ANDROID)
+    #define PLATFORM_MOBILE
+#endif
+
+#if defined(PLATFORM_WEB)
+    #if defined(HEADLESS_ONLY)
+    #define PLATFORM_WEBSERVER
+    #else
+    #define PLATFORM_WEBBROWSER
+    #endif
+#endif
--- nob.c
+++ nob.c
@@ -1,29 +1,20 @@
-// This is your build script. You only need to "bootstrap" it once with `cc -o nob nob.c`
-// (you can call the executable whatever actually) or `cl nob.c` on MSVC. After that every
-// time you run the `nob` executable if it detects that you modifed nob.c it will rebuild
-// itself automatically thanks to NOB_GO_REBUILD_URSELF (see below)
-
-// nob.h is an stb-style library https://github.com/nothings/stb/blob/master/docs/stb_howto.txt
-// What that means is that it's a single file that acts both like .c and .h files, but by default
-// when you include it, it acts only as .h. To make it include implementations of the functions
-// you must define NOB_IMPLEMENTATION macro. This is done to give you full control over where
-// the implementations go.
 #define NOB_IMPLEMENTATION
-
-// Always keep a copy of nob.h in your repo. One of my main pet peeves with build systems like CMake
-// and Autotools is that the codebases that use them naturally rot. That is if you do not actively update
-// your build scripts, they may not work with the latest version of the build tools. Here we basically
-// include the entirety of the source code of the tool along with the code base. It will never get
-// outdated (unless you got no standard compliant C compiler lying around, but at that point why are
-// you trying to build a C project?)
-//
-// (In these examples we actually symlinking nob.h, but this is to keep nob.h-s synced among all the
-// examples)
 #include "includes/nob.h/nob.h"
 
+#include "includes/platform_defs.h"
+
 // Some folder paths that we use throughout the build process.
-#define BUILD_FOLDER "build/"
 #define SRC_FOLDER   "src/"
+
+#if defined(PLATFORM_MACOS)
+#define BUILD_FOLDER "build/macos/"
+#elif defined(PLATFORM_LINUX)
+#define BUILD_FOLDER "build/linux/"
+#elif defined(PLATFORM_WINDOWS)
+#define BUILD_FOLDER "build/windows/"
+#elif defined(PLATFORM_WEB)
+#define BUILD_FOLDER "build/web/"
+#endif
 
 int main(int argc, char **argv)
 {
